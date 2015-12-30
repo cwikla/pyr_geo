7fad1c6d01e87aa61aece9bdb88f8649fca2ebb0
cwikla <cwikla@thegiantpixel.com>
2015-12-30 15:15:11 -0800

    Geo func

---
+++ db/migrate/20151218014224_tgp_geo_func.rb
@@ -0,0 +1,28 @@
+class TgpGeoFunc < ActiveRecord::Migration
+  def up
+    execute <<CMD
+-- Haversine Formula based tgp_geo_distance in miles (constant is diameter of Earth in miles)
+-- Based on a similar PostgreSQL function found here: https://gist.github.com/831833
+-- Updated to use distance formulas found here: http://www.codecodex.com/wiki/Calculate_distance_between_two_points_on_a_globe
+CREATE OR REPLACE FUNCTION tgp_geo_distance(alat double precision, alng double precision, blat double precision, blng double precision)
+  RETURNS double precision AS 
+$BODY$
+SELECT asin(
+  sqrt(
+    sin(radians($3-$1)/2)^2 +
+    sin(radians($4-$2)/2)^2 *
+    cos(radians($1)) *
+    cos(radians($3))
+  ) 
+) * 7926.3352 AS distance;
+$BODY$
+  LANGUAGE sql IMMUTABLE
+  COST 100;
+CMD
+
+  end
+
+  def down
+  end
+end
+
