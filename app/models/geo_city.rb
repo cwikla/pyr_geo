3bbd0027fc18524b81ee518d8ed393f72d0849c4
John Cwikla <cwikla@fwix.com>
2018-01-02 17:20:48 -0800

    First cheezy search function

--- app/models/geo_city.rb
+++ app/models/geo_city.rb
@@ -1,8 +1,4 @@
 class GeoCity < ApplicationRecord
   include Pyr::Geo::Model::City
 
-  def self.search(val, maxCount=50)
-    likeVal = "#{val}%"
-    where(:iso_country => "US").where("latitude is not null").where("city ilike ?", likeVal).order("population desc").limit(maxCount)
-  end
 end
--- app/models/geo_name.rb
+++ app/models/geo_name.rb
@@ -1,8 +1,4 @@
 class GeoName < ApplicationRecord
   include Pyr::Geo::Model::Name
 
-  def self.search(val, maxCount=50)
-    likeVal = "#{val}%"
-    where(:iso_country => "US").where("cluster_latitude is not null").where("name ilike ?", likeVal).limit(maxCount)
-  end
 end
