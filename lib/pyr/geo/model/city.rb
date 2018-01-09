13ee74d3a7c55bc59bc39f31e9a8655ee40e5e6d
John Cwikla <cwikla@fwix.com>
2018-01-09 15:13:14 -0800

    Add search

--- lib/pyr/geo/model/city.rb
+++ lib/pyr/geo/model/city.rb
@@ -9,6 +9,20 @@ module Pyr
       end
     
       module ClassMethods
+        def postal_search(s, **options)
+          country = options[:country] || "US"
+
+          where(iso_country: country).where("postal_code like (?)", "#{s}%")
+        end
+
+        def search(s, **options)
+          country = options[:iso_country] || "US"
+
+          s = s.strip.downcase
+          return postal_search(s) if s =~ /^[0..9]/
+
+          where(iso_country: country).where("lower(name) like (?)", "#{s}%")
+        end
       end
     
     end
