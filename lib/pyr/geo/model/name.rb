f2e6e0ed72bec2261bb580b37fc705176f2b416e
John Cwikla <cwikla@fwix.com>
2018-01-24 14:39:27 -0800

    Fixing up search.

--- lib/pyr/geo/model/name.rb
+++ lib/pyr/geo/model/name.rb
@@ -9,10 +9,16 @@ module Pyr
       end
     
       module ClassMethods
+        def looks_postal?(s)
+          s ||= ""
+          s = s.strip
+          ((s =~ /^[0-9]/) || (s =~ /^[a-zA-Z]\d/))
+        end
+
         def like(gn)
           name = gn.name.downcase
           admin_code_1 = gn.admin_code_1.downcase
-          iso_country = gn.iso_country.downcase
+          iso_country = (gn.iso_country || "US").downcase
 
           where("lower(name) = ?", name).where("lower(admin_code_1) = ?", admin_code_1).where("lower(iso_country) = ?", iso_country)
         end
@@ -26,19 +32,33 @@ module Pyr
         end
 
         def postal_search(s, **options)
+          #country = options[:country] || "US"
+
+          s = (s || "").strip.downcase
+
+          #where(iso_country: country).where("postal_code like (?)", "#{s}%")
+          where("lower(postal_code) like (?)", "#{s.strip.downcase}%")
+        end
+
+        def city_search(s, **options)
           country = options[:country] || "US"
 
-          where(iso_country: country).where("postal_code like (?)", "#{s}%")
+          s = (s || "").strip.downcase
+
+          where(iso_country: country).where("lower(name) like (?)", "#{s.strip.downcase}%")
         end
 
         def search(s, **options)
-          country = options[:country] || "US"
-
           s = s.strip.downcase
-          return postal_search(s) if s =~ /^[0..9]/
+        
+          is_postal = looks_postal?(s)
 
-          where(iso_country: country).where("lower(name) like (?)", "#{s}%")
+          results = postal_search(s, **options) if is_postal
+          results = city_search(s, **options) if !results
+
+          results
         end
+
       end
     
     end
